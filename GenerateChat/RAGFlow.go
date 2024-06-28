979d2dd87f0c35fd1725ec9fa28db8d43b0bf5cf
Naad Borole <naad.borole2000@gmail.com>
2024-06-27 19:49:15 -0700

    Generalize API

--- GenerateChat/RAGFlow.go
+++ GenerateChat/RAGFlow.go
@@ -6,15 +6,20 @@ import (
 	chunkandembed "github.com/Naadborole/TextRAGApi/ChunkAndEmbed"
 	"github.com/Naadborole/TextRAGApi/config"
 	"github.com/tmc/langchaingo/chains"
+	"github.com/tmc/langchaingo/llms"
 	"github.com/tmc/langchaingo/llms/googleai"
 	"github.com/tmc/langchaingo/memory"
 	"github.com/tmc/langchaingo/vectorstores"
 )
 
-var Llm *googleai.GoogleAI
-var RAGChain chains.ConversationalRetrievalQA
+var Llm llms.Model
+var RAGChain chains.Chain
 
 func init() {
+	initializeGoogleAPI()
+}
+
+func initializeGoogleAPI() {
 	var err error
 	Llm, err = googleai.New(context.Background(), googleai.WithAPIKey(config.GetConfigValue("GEMINI_API_KEY")))
 	if err != nil {
