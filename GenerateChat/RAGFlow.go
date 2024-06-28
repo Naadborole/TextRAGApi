a029d61d21d37596a8df666047bca322fe3cbcac
Naad Borole <naad.borole2000@gmail.com>
2024-06-27 20:37:10 -0700

    Added ability to use custom prompts

--- GenerateChat/RAGFlow.go
+++ GenerateChat/RAGFlow.go
@@ -10,6 +10,7 @@ import (
 	"github.com/tmc/langchaingo/llms/cohere"
 	"github.com/tmc/langchaingo/llms/googleai"
 	"github.com/tmc/langchaingo/memory"
+	"github.com/tmc/langchaingo/prompts"
 	"github.com/tmc/langchaingo/vectorstores"
 )
 
@@ -17,7 +18,12 @@ var Llm llms.Model
 var RAGChain chains.Chain
 
 func init() {
-	initializeCohereAPI()
+	var err error
+	Llm, err = cohere.New(cohere.WithToken(config.GetConfigValue("COHERE_API_KEY")))
+	if err != nil {
+		panic(err)
+	}
+	initializeAlternateFlow(Llm)
 }
 
 func initializeGoogleAPI() {
@@ -47,4 +53,26 @@ func GetResponse(text string) string {
 		panic(err)
 	}
 	return res
+}
+
+//A different prompt based flow with history
+
+func initializeAlternateFlow(llm llms.Model) {
+	template := `Use the following pieces of context to answer the question at the end.
+	If you don't know the answer, just say that you don't know, don't try to make up an answer.
+	Use three sentences maximum and keep the answer as concise as possible.
+	Always say "thanks for asking!" at the end of the answer.
+
+	{{.context}}
+
+	Question: {{.question}}
+
+	Helpful Answer:`
+
+	CustomRAGTemplateChain := chains.NewLLMChain(llm, prompts.NewPromptTemplate(template, []string{"context", "question"}))
+	// CustomRAGTemplateChain.OutputKey = "answer"
+	stuffDocChain := chains.NewStuffDocuments(CustomRAGTemplateChain)
+	RAGChain = chains.NewConversationalRetrievalQA(stuffDocChain, chains.LoadCondenseQuestionGenerator(llm),
+		vectorstores.ToRetriever(chunkandembed.Store, 3), memory.NewConversationBuffer())
+
 }
