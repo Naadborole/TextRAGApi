6edbaedc9dac7fae9b223644a65d64475e170fe8
Naad Borole <naad.borole2000@gmail.com>
2024-06-27 22:43:37 -0700

    Added readme.md

--- GenerateChat/RAGFlow.go
+++ GenerateChat/RAGFlow.go
@@ -60,8 +60,7 @@ func GetResponse(text string) string {
 func initializeAlternateFlow(llm llms.Model) {
 	template := `Use the following pieces of context to answer the question at the end.
 	If you don't know the answer, just say that you don't know, don't try to make up an answer.
-	Use three sentences maximum and keep the answer as concise as possible.
-	Always say "thanks for asking!" at the end of the answer.
+	Keep the answer as concise as possible.
 
 	{{.context}}
 
---
+++ readme.md
@@ -0,0 +1,6 @@
+# API for RAG (Retrieval-Augmented Generation)
+
+This project creates API endpoints which enables users to chat with or ask questions based on supplied text/knowledge.
+
+The application makes use of LangChain [langchaingo](https://github.com/tmc/langchaingo) to communicate with LLMs and facilitate RAG.
+
