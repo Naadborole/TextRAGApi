669c6f8d19654bfe3fffe19398ea56bca4cb1349
Naad Borole <naad.borole2000@gmail.com>
2024-06-27 18:49:03 -0700

    Cleaned up code

--- main.go
+++ main.go
@@ -8,52 +8,19 @@ import (
 
 	chunkandembed "github.com/Naadborole/TextRAGApi/ChunkAndEmbed"
 	generatechat "github.com/Naadborole/TextRAGApi/GenerateChat"
-	"github.com/Naadborole/TextRAGApi/models"
 	"github.com/google/uuid"
 
-	"github.com/Naadborole/TextRAGApi/database"
 	"github.com/gin-gonic/gin"
 	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
 	server := gin.Default()
-	server.GET("/index/:id", getIndex)
-	server.GET("/indexList", getIndexList)
-	server.POST("/index", postIndex)
 	server.POST("/upload", postUpload)
 	server.POST("/embedAndStore", postEmbed)
 	server.POST("/queryDoc", postQueryDoc)
 	server.POST("/queryChat", postQueryChat)
 	server.Run()
-}
-
-func getIndex(context *gin.Context) {
-	id := context.Param("id")
-	index, err := database.GetIndex(id)
-	if err != nil {
-		context.JSON(http.StatusNotFound, gin.H{"message": "Index not found"})
-		return
-	}
-	context.JSON(http.StatusFound, index)
-}
-
-func getIndexList(context *gin.Context) {
-	context.JSON(http.StatusOK, database.GetIndexList())
-}
-
-func postIndex(context *gin.Context) {
-	var index models.Index
-	if err := context.ShouldBindJSON(&index); err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
-		return
-	}
-	status, err := database.AddIndex(index)
-	if err != nil || !status {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
-		return
-	}
-	context.JSON(http.StatusCreated, index)
 }
 
 func postUpload(context *gin.Context) {
